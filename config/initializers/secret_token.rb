9e336f7e223372c3fd0f60ce4a76b511f2725996
noelrojo <noelrojo@hotmail.com>
2014-03-06 13:26:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skillflare::Application.config.secret_key_base = 'ed777eb547f85159fed39949130899434e6fd420efd1a74ac08fcf5bc0b65f5e4e921ffaa3aa1afd65d028562fdabb7b5a42df9e7483bf59df8ad91f1ab8c545'
